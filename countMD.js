0e027403fc5e289850858a20a66342c5af070ddd
macsu <macsu@tencent.com>
2020-09-07 15:27:28 +0800

    feat:增加markdown字数统计功能

---
+++ countMD.js
@@ -0,0 +1,70 @@
+/**
+ * 统计Markdown字数和行数
+ * @param pathStr
+ * @constructor
+ */
+function CountMD(pathStr = '.'){
+  let fs = require('fs')
+  let path = require('path')
+
+// 获取命令行参数
+  let parm = process.argv.splice(2)
+// 第一个参数是路径
+  let rootPath = pathStr
+// 后面的所有参数都是文件后缀
+  let types = ['.md','.MD','.markdown','.MarkDown','.Markdown','.MARKDOWN']
+// 需要过滤的文件夹
+  let filter = ['/node_modules']
+// 统计结果
+  let num = 0
+
+// 文字总结果
+  let wordsCount = 0
+
+// 获取行数
+  async function line(path) {
+    let rep = await fs.readFileSync(path)
+    rep = rep.toString()
+    let len = rep.match(
+      /([\u0800-\u4e00]+?|[\u4e00-\u9fa5]+?|[a-zA-Z0-9]+|[，："、【】。；「」《》？！…（）])/g
+    ).length
+    wordsCount += len
+    let lines = rep.split('\n')
+    console.log(path+' '+len+'字'+' '+lines.length+'行')
+    num += lines.length
+  }
+
+// 递归所有文件夹统计
+  async function start(pt) {
+    let files = fs.readdirSync(pt)
+    files
+    .map(file => {
+      return `${pt}/${file}`
+    })
+    .forEach(file => {
+      let stat = fs.statSync(file)
+      if (stat.isDirectory()) {
+        for (let i = 0; i < filter.length; i++) {
+          if (pt.search(`${filter[i]}`)!==-1){
+            return
+          }
+        }
+        start(file)
+        return
+      }
+      let ext = path.extname(file)
+      if (types.indexOf(ext) !== -1) {
+        line(file)
+      }
+    })
+  }
+
+  (async () => {
+    await start(rootPath)
+    console.log(`总字数：${wordsCount}`)
+    console.log(`总行数：${num}`)
+  })()
+}
+
+// CountMD()
+module.exports =  CountMD
--- index.js
+++ index.js
@@ -1,59 +1,72 @@
 #! /usr/bin/env node
-var child_process = require('child_process');
-var argv = require('yargs')
-    .command("npm源", '设置npm源为官方源', function (yargs) {
-        console.log('执行命令：💤')
-        console.log('npm config set registry https://registry.npmjs.org');
-        child_process.execSync('npm config set registry https://registry.npmjs.org');
-        console.log('✅ 将npm源更改为：官方npm源📁');
-    })
-    .command("taobao源", '设置npm源为淘宝源', function (yargs) {
-        console.log('执行命令：💤')
-        console.log('npm config set registry https://registry.npm.taobao.org');
-        child_process.execSync('npm config set registry https://registry.npm.taobao.org');
-        console.log('✅ 将npm源更改为：淘宝源🍑');
-    })
-    .command("publish", '发布包到npm官方', function (yargs) {
-        yargs = yargs
-            .demandCommand(1, '❌ 你必须在publish后面加上新版本号，如X.X.X');
-        console.log('执行命令：💤');
-        console.log('npm config set registry https://registry.npmjs.org');
-        child_process.execSync('npm config set registry https://registry.npmjs.org');
-        console.log('npm version ' + yargs.argv._[1]);
-        child_process.execSync('npm version ' + yargs.argv._[1]);
-        console.log('npm publish');
-        child_process.execSync('npm publish');
-        console.log('npm config set registry https://registry.npm.taobao.org');
-        child_process.execSync('npm config set registry https://registry.npm.taobao.org');
-        console.log('✅ 发布成功！🎉');
-    })
-    .command("docs", '启动docsify', function (yargs) {
-        console.log('执行命令：💤')
-        console.log('docsify serve docs');
-        child_process.execSync('docsify serve docs',{stdio: 'inherit'});
-        console.log('✅ docsify启动成功！ 🚗');
-    })
-    .command("push", '提交主分支到远程仓库', function (yargs) {
-        yargs = yargs
-            .demandCommand(1, '❌ 你必须在push后面加上commit说明');
-        console.log('执行命令：💤')
-        console.log('git add .');
-        child_process.execSync('git add .');
-        console.log('git commit -m '+yargs.argv._[1]+' .');
-        child_process.execSync('git commit -m "'+yargs.argv._[1]+'" .');
-        console.log('git push -u origin master');
-        child_process.execSync('git push -u origin master');
-        console.log('✅ git push 成功 🚗');
-    })
-    .usage(`      _                   _      _                 
+let CountMD = require('./countMD')
+var child_process = require('child_process')
+var argv = require('yargs').
+  command('npm源', '设置npm源为官方源', function (yargs) {
+    console.log('执行命令：💤')
+    console.log('npm config set registry https://registry.npmjs.org')
+    child_process.execSync(
+      'npm config set registry https://registry.npmjs.org')
+    console.log('✅ 将npm源更改为：官方npm源📁')
+  }).
+  command('taobao源', '设置npm源为淘宝源', function (yargs) {
+    console.log('执行命令：💤')
+    console.log('npm config set registry https://registry.npm.taobao.org')
+    child_process.execSync(
+      'npm config set registry https://registry.npm.taobao.org')
+    console.log('✅ 将npm源更改为：淘宝源🍑')
+  }).
+  command('publish', '发布包到npm官方', function (yargs) {
+    yargs = yargs.demandCommand(1, '❌ 你必须在publish后面加上新版本号，如X.X.X')
+    console.log('执行命令：💤')
+    console.log('npm config set registry https://registry.npmjs.org')
+    child_process.execSync(
+      'npm config set registry https://registry.npmjs.org')
+    console.log('npm version ' + yargs.argv._[1])
+    child_process.execSync('npm version ' + yargs.argv._[1])
+    console.log('npm publish')
+    child_process.execSync('npm publish')
+    console.log('npm config set registry https://registry.npm.taobao.org')
+    child_process.execSync(
+      'npm config set registry https://registry.npm.taobao.org')
+    console.log('✅ 发布成功！🎉')
+  }).
+  command('docs', '启动docsify', function (yargs) {
+    console.log('执行命令：💤')
+    console.log('docsify serve docs')
+    child_process.execSync('docsify serve docs', { stdio: 'inherit' })
+    console.log('✅ docsify启动成功！ 🚗')
+  }).
+  command('push', '提交主分支到远程仓库', function (yargs) {
+    yargs = yargs.demandCommand(1, '❌ 你必须在push后面加上commit说明')
+    console.log('执行命令：💤')
+    console.log('git add .')
+    child_process.execSync('git add .')
+    console.log('git commit -m ' + yargs.argv._[1] + ' .')
+    child_process.execSync('git commit -m "' + yargs.argv._[1] + '" .')
+    console.log('git push -u origin master')
+    child_process.execSync('git push -u origin master')
+    console.log('✅ git push 成功 🚗')
+  }).
+  command('count', '统计Markdown文字字数（后可以跟上需要统计的目录绝对路径，或直接省略或.以遍历整个相对目录）', function (yargs) {
+    console.log('执行命令：💤')
+    let currPath
+    if (yargs.argv._[1] === '.' || yargs.argv._[1] === undefined) {
+      currPath = child_process.execSync('pwd').toString('utf8').trim()
+    } else {
+      currPath = yargs.argv._[1]
+    }
+    CountMD(currPath)
+  }).
+  usage(`      _                   _      _                 
      | |  _   _   _ __   | | __ (_)  _ __     __ _ 🚀☁️
   _  | | | | | | | '_ \\  | |/ / | | | '_ \\   / _\` |
  | |_| | | |_| | | | | | |   <  | | | | | | | (_| |
   \\___/   \\__,_| |_| |_| |_|\\_\\ |_| |_| |_|  \\__, |
                                              |___/ 
-    `)
-    .usage('欢迎使用junking,提升你的效率🌞,不来试试吗？')
-    .help('h')
-    .alias('h', 'help')
-    .epilog('copyright 🌀苏一恒')
-    .argv;
+    `).
+  usage('欢迎使用junking,提升你的效率🌞,不来试试吗？').
+  help('h').
+  alias('h', 'help').
+  epilog('copyright 🌀苏一恒').argv
+
